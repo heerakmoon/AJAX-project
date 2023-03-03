c5882f3e212d66a65ef1240ee7f84f47c53d5d2a
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-03 12:06:59 +0000

    submitted ajax request for a random quote

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 // var $yesterdaysQuote = document.querySelector('#yesterdays-quote');
-// var $quoteOfDay = document.querySelector('#new-daily-quote');
+var $quoteOfDay = document.querySelector('#new-daily-quote');
 
-// var xhr = new XMLHttpRequest();
-// xhr.open('GET', 'https://animechan.vercel.app/api/random');
-// xhr.responseType = 'json';
-// xhr.addEventListener('load', function () {
-//   console.log(xhr.status);
-//   console.log(xhr.response);
-//   var $newQuote = document.createElement('h4');
-//   $newQuote.textContent = xhr.response.quote;
-//   $quoteOfDay.appendChild($newQuote);
-// });
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://animechan.vercel.app/api/random');
+xhr.responseType = 'json';
+xhr.addEventListener('load', function () {
+  // console.log(xhr.status);
+  // console.log(xhr.response);
+  var $newQuote = document.createElement('h4');
+  $newQuote.textContent = '"' + xhr.response.quote + '"';
+  $quoteOfDay.appendChild($newQuote);
+});
+
+xhr.send();
